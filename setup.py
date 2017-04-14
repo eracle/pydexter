80d468f4900ae2609c7c0bf09e46f503ec600e61
eracle <eracle@posteo.eu>
2017-04-14 02:17:01 +0200

    modified setup2

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ def readme():
         return f.read()
 
 setup(name='pydexter',
-      #version='0.1',
       description='A Python client for the Dexter REST API',
       long_description=readme(),
       classifiers=[
@@ -18,18 +17,4 @@ setup(name='pydexter',
       ],
       keywords=['dexter', 'entity linking', 'wikification'],
       url='http://github.com/giacbrd/pydexter',
-      author='Giacomo Berardi',
-      author_email='barnets@gmail.com',
-      license='Apache',
-      packages=find_packages('src'),
-      package_dir={'': 'src'},
-      install_requires=[
-          'requests',
-      ],
-      #test_suite='nose.collector',
-      #tests_require=['nose', 'nose-cover3'],
-      #entry_points={
-      #    'console_scripts': ['funniest-joke=funniest.command_line:main'],
-      #},
-      include_package_data=True,
-      zip_safe=False)
+	)
