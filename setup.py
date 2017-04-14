60cbf4dcaca052568b931015bcea5c3e21b376be
eracle <eracle@posteo.eu>
2017-04-14 02:43:49 +0200

    fixed setup 3

--- setup.py
+++ setup.py
@@ -1,20 +1,18 @@
-__author__ = 'Giacomo Berardi <barnets@gmail.com>'
+from distutils.core import setup
 
-from setuptools import setup, find_packages
-
-readme_file='README.md'
-def readme():
-    with open(readme_file) as f:
-        return f.read()
-
-setup(name='pydexter',
-      description='A Python client for the Dexter REST API',
-      long_description=readme(),
-      classifiers=[
+setup(
+    name='pydexter',
+    version='0.1.0',
+    packages=[''],
+    url='http://github.com/giacbrd/pydexter',
+    license='Apache',
+    author='Giacomo Berardi',
+    author_email='barnets@gmail.com',
+    description='A Python client for the Dexter REST API',
+    classifiers=[
         'License :: OSI Approved :: Apache License',
         'Programming Language :: Python',
         'Topic :: Text Processing :: Linguistic',
       ],
-      keywords=['dexter', 'entity linking', 'wikification'],
-      url='http://github.com/giacbrd/pydexter',
-	)
+    keywords=['dexter', 'entity linking', 'wikification'],
+)
